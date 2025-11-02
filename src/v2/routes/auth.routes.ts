0f7ca79afd1fe2ad511d05d6b403a9f2069dc902
ChadiEC <1985081@collegemv.qc.ca>
2025-11-02 16:04:19 -0500

    J'ai oublier de mettre le middleware loginLimiter dans mon /auth.routes.ts dans ma route login

--- src/v2/routes/auth.routes.ts
+++ src/v2/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { loginLimiter } from "../middlewares/rateLimit";
 import { registerController, loginController } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", registerController);
-router.post("/login", loginController);
+router.post("/login", loginLimiter,loginController);
 
 export default router;
